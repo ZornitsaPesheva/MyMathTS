c24a6e4a8d7c4cea6851ae44decc164152fb4da8
ZornitsaSerbezova <pesheva@gmail.com>
2020-06-15 09:55:51 +0300

    commit

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare class MyMathTS {
+    constructor(x: number, y: number);
+ sum(x: number, y: number) : number;
+} 
+
+//export = MyMathTS;
+export default MyMathTS;
+
---
+++ index.js
@@ -0,0 +1,7 @@
+var MyMathTS = function(a, b){
+	this.sum = a + b;
+};
+
+//exports = MyMathTS;
+
+export default MyMathTS;
